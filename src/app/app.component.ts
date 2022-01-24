4aca57f80cb71241b371680193f317a322a30d47
Michael Mogessie <michaelmogessie@gmail.com>
2022-01-24 12:58:36 -0500

    LIBRARY-CATALOG-DEMO-APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'library-catalog-demo-app';
+
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {
+
+  }
+
+  loggedIn: any = localStorage.getItem('user') ? true : false;
+  user: any = localStorage.getItem('user')
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter an email';
+    }
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  ngOnInit(): void {
+    if (this.loggedIn) {
+      this.loggedIn = false
+    }
+  }
+
+  login() {
+    let credentials = { 'email': this.email.value, 'password': this.password.value }
+    this.http.post('http://localhost:8885/login', credentials, { observe: 'response' }).subscribe((response: any) => {
+      let user = response.body
+      user.books = JSON.parse(user.books)
+      this.user = user
+      localStorage.setItem('auth_token', response.headers.get('Auth_token'))
+      localStorage.setItem('user', JSON.stringify(this.user))
+      this.loggedIn = true
+      this._snackBar.open('Welcome ' + this.user.first_name, 'OK', {
+        duration: 3000
+      });
+    },
+      (error) => {
+        this._snackBar.open('Invalid email or password', 'OK', {
+          duration: 3000
+        });
+      });
+
+  }
+
+  saveBook(book: any) {
+    this.http.post('http://localhost:8885/books/add', book, this.getRequestOptions()).subscribe((response: any) => {
+      this._snackBar.open('Success', 'OK', {
+        duration: 3000
+      });
+    },
+      (error) => {
+        this._snackBar.open('Error, invalid book info.', 'OK', {
+          duration: 3000
+        });
+      });
+  }
+
+  getRequestOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('auth_token')
+      }),
+    }
+  }
+
+  removeBook(book: any) {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MaterialsModule } from './materials/materials.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    CoreModule,
+    MaterialsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
